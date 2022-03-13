1d9f1714502e3bd2c44a6d11f2ebe40a8628c735
ivanov-dk <40234590+ivanov-dk@users.noreply.github.com>
2022-03-13 20:22:27 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+      });
+
+    $( "#tabs" ).tabs();
+});
