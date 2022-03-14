9da6080023092a65757c5235ff74629448616582
ivanov-dk <40234590+ivanov-dk@users.noreply.github.com>
2022-03-14 18:22:12 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -9,3 +9,25 @@ $(document).ready(function(){
 
     $( "#tabs" ).tabs();
 });
+
+// EasterEgg
+
+let text = document.querySelector('#text')
+let close = document.querySelector('.close')
+let count = 0
+
+text.addEventListener('click', function() {
+    if (count > 10) {
+        clickEasterEgg()
+        return count = 0
+    }
+    return count++
+})
+
+function clickEasterEgg() {
+    document.querySelector('.hiddenBlock').classList.add('d-flex')
+}
+
+close.addEventListener('click', function() {
+    document.querySelector('.hiddenBlock').classList.remove('d-flex')
+})
